f6b8241e739a74e03d2fb4e5743bf88c6864516c
Noel King <noelking@gmail.com>
2015-02-14 22:40:26 +0000

    Updated version number for build pipeline

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'coolestprojects@coderdojo.com'
 license          'MIT'
 description      'Deploys Play Application'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version          '0.0.1'
 
 depends 'cp_baseconfig'
 depends 'cp_java'
