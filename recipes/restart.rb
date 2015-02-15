ce3f78ff562e9575ca2aa1bd8c0a5651345f9a8e
Noel King <noelking@gmail.com>
2015-02-15 17:21:18 +0000

    Add debugging

--- recipes/restart.rb
+++ recipes/restart.rb
@@ -1,6 +1,9 @@
+appName="#{node[:play_app][:app_dir]}"
+puts 'app dir #{appName}'
+
 bash "playframework restart" do
   user "root"
   code <<-EOH
-  /etc/init.d/#{node[:play_app][:app_dir]} restart
+  /etc/init.d/#{appName} restart
   EOH
 end
--- recipes/start.rb
+++ recipes/start.rb
@@ -1,6 +1,6 @@
 bash "playframework start" do
   user "root"
   code <<-EOH
-  /etc/init.d/#{node[:play_app][:app_name]} start
+  /etc/init.d/#{node[:play_app][:app_dir]} start
   EOH
 end
