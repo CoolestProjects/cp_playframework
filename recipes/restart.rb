a15aeef60725387378345bb4f712c683e2ca6488
Noel King <noelking@gmail.com>
2015-02-15 16:25:26 +0000

    Updated folder name

--- recipes/restart.rb
+++ recipes/restart.rb
@@ -1,6 +1,6 @@
 bash "playframework restart" do
   user "root"
   code <<-EOH
-  /etc/init.d/#{node[:play_app][:application_name]} restart
+  /etc/init.d/#{node[:play_app][:app_dir]} restart
   EOH
 end
--- recipes/start.rb
+++ recipes/start.rb
@@ -1,6 +1,6 @@
-bash "playframework restart" do
+bash "playframework start" do
   user "root"
   code <<-EOH
-  /etc/init.d/#{node[:play_app][:application_name]} start
+  /etc/init.d/#{node[:play_app][:app_name]} start
   EOH
 end
